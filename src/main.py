a97ceac478c48996de59ac2e891b3568c22780ce
valiantlynx <Gormery.Wanjiru@aenergi.no>
2025-08-07 11:02:46 +0200

    start setup for streaming route

--- src/main.py
+++ src/main.py
@@ -1,16 +1,41 @@
 from typing import Union
+import asyncio
+
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+import debugpy
 
 app = FastAPI()
- 
-import debugpy
 debugpy.listen(("0.0.0.0", 5678))
+
+# Enable CORS for all/specific origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World ass wiper"}
+    return {"Welcome": "to the AI Alchemy API"}
 
+async def token_generator(content: str, streamer):
+    pass
+class QueueCallbackHandler:
+    pass
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/api/invoke")
+async def invoke(content: str):
+    queue: asyncio.Queue[Union[str, bytes]] = asyncio.Queue()
+    streamer = QueueCallbackHandler(queue)
+    
+    return StreamingResponse(
+        token_generator(content, streamer),
+        media_type="text/event-stream",
+        headers={"Cache-Control": "no-cache",
+                 "Connection": "keep-alive"}
+    )   
+    
